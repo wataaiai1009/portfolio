af9ac3dafb00a4fc77a8f4060eff3c08eb4b9fcf
wataaiai1009 <169005311+wataaiai1009@users.noreply.github.com>
2024-06-30 13:59:24 +0900

    Add files via upload

--- pf/main.js
+++ pf/main.js
@@ -5,3 +5,36 @@ btn.addEventListener('click', ()=>{
   btn.setAttribute('disabled' , true);
 });
 
+
+
+
+const cp = document.getElementById('catchCopy').animate(
+	
+	[
+		{ transform: 'translateY(0) scale(1)' },
+		{ transform: 'translateY(-140px)' },
+		{ transform: 'scale(1)' },
+		{ transform: 'scale(1.3, 0.9)' },
+		{ transform: 'translateY(0) scale(1)' }
+	],
+	{
+		duration: 2000,
+		iterations: Infinity
+	}
+);
+
+
+	const kaiwa = document.getElementById('kaiwa01').animate(
+
+	  [
+			{ transform: 'translateY(0) scale(1)' },
+			{ transform: 'translateY(-90px)' },
+			{ transform: 'scale(1)' },
+			{ transform: 'scale(1.0, 1.0)' },
+			{ transform: 'translateY(0) scale(1)' }
+		],
+		{
+			duration: 2000,
+			// iterations: Infinity
+		}
+	);
