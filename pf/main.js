9a2792f563ad20dcbd9277efe2d7bdf7a960010c
wataaiai1009 <169005311+wataaiai1009@users.noreply.github.com>
2024-07-17 13:44:16 +0900

    Add files via upload

--- pf/main.js
+++ pf/main.js
@@ -1,40 +1,74 @@
-const btn = document.getElementById('send');
+// 送信ボタン
+const btn = document.querySelector('.send');
 
 btn.addEventListener('click', ()=>{
-  btn.textContent = '送信しました' ;
+  btn.innerHTML = '送信しました' ;
   btn.setAttribute('disabled' , true);
 });
 
 
-
-
-const cp = document.getElementById('catchCopy').animate(
+// キャッチコピーのイラストの動き
+const cp = document.querySelector('.people').animate(
 	
 	[
 		{ transform: 'translateY(0) scale(1)' },
 		{ transform: 'translateY(-140px)' },
 		{ transform: 'scale(1)' },
-		{ transform: 'scale(1.3, 0.9)' },
+		{ transform: 'scale(1.1, 0.9)' },
 		{ transform: 'translateY(0) scale(1)' }
 	],
 	{
-		duration: 2000,
+		 duration: 3000,
+		 iterations: Infinity
+	}
+);
+
+
+// お問い合わせのイラストの動き
+const pan01 = document.querySelector('.pan0102').animate(
+	[
+		{
+			offset: 0.00,
+			transform: 'translate(0, 0)'
+		},
+		{
+			offset: 0.05,
+			transform: 'translate(-3%, 0)'
+		},
+		
+		{
+			offset: 0.10,
+			transform: 'translate(3%, 0)'
+		},
+		{
+			offset: 0.15,
+			transform: 'translate(-3%, 0)'
+		},
+		{
+			offset: 0.20,
+			transform: 'translate(3%, 0)'
+		},
+		{
+			offset: 0.25,
+			transform: 'translate(-3%, 0)'
+		},
+		{
+			offset: 0.30,
+			transform: 'translate(0, 0)'
+		},
+		{
+			offset: 1.00,
+			transform: 'translate(0, 0)'
+		}
+	],
+
+	{
+		duration: 6000,
 		iterations: Infinity
 	}
 );
 
 
-	const kaiwa = document.getElementById('kaiwa01').animate(
 
-	  [
-			{ transform: 'translateY(0) scale(1)' },
-			{ transform: 'translateY(-90px)' },
-			{ transform: 'scale(1)' },
-			{ transform: 'scale(1.0, 1.0)' },
-			{ transform: 'translateY(0) scale(1)' }
-		],
-		{
-			duration: 2000,
-			// iterations: Infinity
-		}
-	);
+
+	
